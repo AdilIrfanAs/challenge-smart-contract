bf21a9d79f5e11b0b516379963df36be95012fa6
Adil Irfan <adil.irfan.as@gmail.com>
2022-12-05 14:12:30 +0500

    Test sushi token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,29 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("DEPLOYER: ", deployer.address);
 
-  const [deployer,] = await ethers.getSigners();
-  console.log('DEPLOYER: ',deployer.address);
 
+  console.log(".......... SushiToken ..........");
   const SushiToken = await hre.ethers.getContractFactory("SushiToken");
   const sushiToken = await SushiToken.deploy();
   await sushiToken.deployed();
-  try{await hre.run("verify:verify", {address: sushiToken.address});}catch(e){}
-  console.log("SushiToken deployed:", sushiToken.address);
+  try {
+    await hre.run("verify:verify", 
+    { 
+      address: sushiToken.address,
+      contract: "contracts/SushiToken.sol:SushiToken",
+      constructorArguments: []
+    });
+  } catch (e) {
+    console.log("Verification error:", e.message);
+  }
 
+
+
+  console.log(".......... FINAL DEPLOYED CONTRACT ADDRESSES ..........");
+  console.log("SushiToken deployed:", sushiToken.address);
 }
 
 main()
